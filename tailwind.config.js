3030163ee8ae65342aa2d7e9d34ed94c7ca5ebf9
DAC <71835941+corvad@users.noreply.github.com>
2023-02-13 16:51:18 +0000

    Added theme and navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
-  theme: {
-    extend: {},
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+        
+"primary": "#60a5fa",
+        
+"secondary": "#86efac",
+        
+"accent": "#6366f1",
+        
+"neutral": "#bfdbfe",
+        
+"base-100": "#1f2937",
+        
+"info": "#a78bfa",
+        
+"success": "#15803d",
+        
+"warning": "#fb923c",
+        
+"error": "#ef4444",
+        },
+      },
+    ],
   },
-  plugins: [require("daisyui")],
+  plugins: [
+    require('daisyui'),
+  ],
 }
