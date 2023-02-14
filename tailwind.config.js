e43c8a4d3a08b0653c4c82138003d0c628c014a3
DAC <71835941+corvad@users.noreply.github.com>
2023-02-14 02:41:56 +0000

    Added example and have code ready to show for hand on.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
-  plugins: [
-    require('daisyui'),
-  ],
-}
+  daisyui: {
+    themes: [
+      {
+      capitol: {
+          "primary": "#0284c7",
+          "secondary": "#9ca3af",
+          "accent": "#60a5fa",
+          "neutral": "#bfdbfe",
+          "base-100": "#f3f4f6",
+          "info": "#8AB1E0",
+          "success": "#18B466",
+          "warning": "#C9A508",
+          "error": "#DC3D32",
+          },
+        },
+      ],
+    },
+    plugins: [
+      require('daisyui'),
+    ],
+  }
