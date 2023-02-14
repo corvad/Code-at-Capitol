66c38bd60a9e9bc4458b12fb66e81ce1e95a2655
DAC <71835941+corvad@users.noreply.github.com>
2023-02-14 00:32:28 +0000

    Worked more on the website for congress.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
-  daisyui: {
-    themes: [
-      {
-        mytheme: {
-        
-"primary": "#60a5fa",
-        
-"secondary": "#86efac",
-        
-"accent": "#6366f1",
-        
-"neutral": "#bfdbfe",
-        
-"base-100": "#1f2937",
-        
-"info": "#a78bfa",
-        
-"success": "#15803d",
-        
-"warning": "#fb923c",
-        
-"error": "#ef4444",
-        },
-      },
-    ],
-  },
   plugins: [
     require('daisyui'),
   ],
